1a2c05a298a69ec3b034d55d44eca489dd8618eb
fitblip <fitblip@gmail.com>
2018-12-30 03:37:13 -0500

    Make the server standalone (no external assets needed)
    Also bumps some versions and adds a stats endpoint
    
    
    Former-commit-id: 354138ead248696ced5264e089d234eb05e8c194

--- lib/application.ex
+++ lib/application.ex
@@ -2,7 +2,19 @@ defmodule Certstream do
   use Application
 
   def start(_type, _args) do
-    supervisor_info = Certstream.Supervisor.start_link(name: CertstreamSupervisor)
+    children = [
+      # Web services
+      Certstream.WebsocketServer,
+
+      # Agents
+      Certstream.ClientManager,
+      Certstream.CertifcateBuffer,
+
+      # Watchers
+      {DynamicSupervisor, name: WatcherSupervisor, strategy: :one_for_one}
+    ]
+
+    supervisor_info = Supervisor.start_link(children, strategy: :one_for_one)
 
     Certstream.CTWatcher.start_and_link_watchers(name: WatcherSupervisor)
 
