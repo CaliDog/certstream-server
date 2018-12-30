e58894d7d7740463ac5e985c2a43269e36bfcfc4
fitblip <fitblip@gmail.com>
2018-12-30 03:34:45 -0500

    re-add HTML frontend to the server repo
    Former-commit-id: 094c85ed713de56ab62e87cebf1b11f5fcab54e6

---
+++ html/src/vues/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <frontpage></frontpage>
+  </div>
+</template>
+
+<script>
+import Frontpage from './Frontpage.vue'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  },
+  components: {
+      Frontpage
+  }
+}
+</script>
+
+<style lang="scss">
+  h1, h2, h3, h4, h5, p {
+    font-family: 'Open Sans', sans-serif;
+  }
+  .container{
+    max-width: none;
+    width: auto;
+  }
+</style>
---
+++ html/webpack.config.js
@@ -0,0 +1,119 @@
+const path = require('path');
+
+const webpack = require('webpack');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+      path: path.resolve(__dirname, './dist/'),
+      filename: 'static/build.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+            // the "scss" and "sass" values for the lang attribute to the right configs here.
+            // other preprocessors should work out of the box, no loader config like this necessary.
+            'scss': 'vue-style-loader!css-loader!sass-loader',
+            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+          }
+          // other vue-loader options go here
+        }
+      },
+      {
+        test: /\.(scss|sass)$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&minetype=application/font-woff",
+        options: {name: 'static/font/[name].[ext]?[hash]'}
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader",
+        options: {name: 'static/font/[name].[ext]?[hash]'}
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'static/img/[name].[ext]?[hash]'
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'image-webpack-loader',
+        options: {
+          optipng: {
+            enabled: true,
+            optimizationLevel: 7,
+          },
+          bypassOnDebug: true
+        }
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: false,
+    host: '0.0.0.0',
+    disableHostCheck: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+        'process.env': {
+            NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+        }
+    }),
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    }),
+  ]
+};
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+    module.exports.output.publicPath = "https://storage.googleapis.com/certstream-prod/";
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: false,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
