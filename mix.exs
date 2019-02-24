88b3e7a66e0b6e092c1a72a5b60e794ec49c7152
Ryan Sears <ryan@calidog.io>
2019-02-23 23:30:03 -0500

    Add watcher stats

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Certstream.Mixfile do
   def project do
     [
       app: :certstream,
-      version: "1.1.1",
+      version: "1.1.2",
       elixir: "~> 1.6",
       start_permanent: Mix.env == :prod,
       deps: deps(),
