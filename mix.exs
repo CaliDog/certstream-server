3a39fceef991d895c6196d6338f529930fcaf239
Ryan Sears <ryan@calidog.io>
2019-12-19 22:58:13 -0500

    Add mix.lock and update deps

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule Certstream.Mixfile do
       deps: deps(),
       aliases: aliases(),
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
     ]
   end
 
