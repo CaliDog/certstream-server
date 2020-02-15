9883c2d2338c4ca40f0275483ddb65129475f029
Ryan Sears <ryan@calidog.io>
2020-02-15 13:03:25 -0500

    Bump version
    Former-commit-id: 0c591ce85cb0090fab5b753c214b17171f9d5fc1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Certstream.Mixfile do
   def project do
     [
       app: :certstream,
-      version: "1.2.0",
+      version: "1.3.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env == :prod,
       deps: deps(),
