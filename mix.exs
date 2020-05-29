712fa8f4dc706e3b6e9d8e3be2f1baf2ba849d7b
Manabu Niseki <manabu.niseki@gmail.com>
2020-05-30 08:16:53 +0900

    Update elixir dependencies

--- mix.exs
+++ mix.exs
@@ -6,33 +6,38 @@ defmodule Certstream.Mixfile do
       app: :certstream,
       version: "1.4.1",
       elixir: "~> 1.6",
-      start_permanent: Mix.env == :prod,
+      start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases(),
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
   def application do
     [
       extra_applications: [:honeybadger, :logger],
-      mod: {Certstream, []},
+      mod: {Certstream, []}
     ]
   end
 
   defp deps do
     [
-      {:cowboy, "~> 2.2"},
-      {:easy_ssl, "~> 1.1.0"},
-      {:honeybadger, "~> 0.1"},
-      {:httpoison, "~> 1.0"},
-      {:instruments, git: "https://github.com/discordapp/instruments.git"},
-      {:jason, "~> 1.0"},
-      {:number, "~> 0.5.5"},
-      {:pobox, "~> 1.0.2"},
-      {:credo, "~> 0.9.0-rc1", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.8", only: :test}
+      {:cowboy, "~> 2.7"},
+      {:easy_ssl, "~> 1.1"},
+      {:honeybadger, "~> 0.14"},
+      {:httpoison, "~> 1.6"},
+      {:instruments, "~> 1.1"},
+      {:jason, "~> 1.2"},
+      {:number, "~> 1.0"},
+      {:pobox, "~> 1.2"},
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.13", only: :test}
     ]
   end
 
